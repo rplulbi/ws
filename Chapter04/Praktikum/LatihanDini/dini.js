162019af6741fbec9d0dd5a123e7f1508b45f797
Dini Lubis <0606ayl@gmail.com>
2023-03-23 11:27:53 +0700

    Tugas Chapter 04

--- Chapter04/Praktikum/LatihanDini/dini.js
+++ Chapter04/Praktikum/LatihanDini/dini.js
@@ -8,8 +8,10 @@ var requestOptions = {
 };
 
 hasil=""
+txt=""
+txt1=""
 
-fetch("https://cat-fact.herokuapp.com/facts", requestOptions)
+fetch("https://gorest.co.in/public/v2/users", requestOptions)
   .then(response => response.text())
   .then(result => tampilkan(result))
   .catch(error => console.log('error', error));
@@ -17,6 +19,28 @@ fetch("https://cat-fact.herokuapp.com/facts", requestOptions)
 function tampilkan(result){
   console.log(result);
   hasil=JSON.parse(result);
-
-  //document.getElementById("nama").innerHTML(result);
+  txt=hasil.forEach(isitabel);
+//   document.getElementById("nama").innerHTML(result);
 }
+
+function isitabel(value){
+    const oke = "cat:"
+    const texr = "source:"
+    const user = "user"
+    const ter = "updatedAt:"
+    const delet = "delete:"
+    const used = "user:"
+    txt= txt+trnyatabel.replace("#TEXT#","\nnama:"+value._id);
+    txt= txt+trnyatabel.replace("#TEXT#",oke+value.text+"");
+    txt= txt+trnyatabel.replace("#TEXT#",texr+value.source+"");
+    txt= txt+trnyatabel.replace("#TEXT#",ter+value.source+"");
+    txt= txt+trnyatabel.replace("#TEXT#",user+value.user+"");
+    txt= txt+trnyatabel.replace("#TEXT#",delet+value.user+"");
+    txt= txt+trnyatabel.replace("#TEXT#",used+value.used+"");
+
+    document.getElementById("konten").innerHTML=txt;
+    }
+    trnyatabel=`
+    <div class="font-medium text-gray-700" id="name">#TEXT#</div>
+  `
+    
